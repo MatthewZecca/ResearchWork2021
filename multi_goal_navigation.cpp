295c278c8f0dc0a6299855d2f34a5b31da32a384
MatthewZecca <87440798+MatthewZecca@users.noreply.github.com>
2021-07-14 13:39:24 -0400

    Add files via upload
    Work-in-progress. Haven't gotten to build in the catkin workspace and test yet. This is what the general steps will look like in handling multiple goals.

---
+++ multi_goal_navigation.cpp
@@ -0,0 +1,93 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+
+move_base_msgs::MoveBaseGoal goalList [50] = {0};
+bool isNavigating = false;
+
+void addGoal(const geometry_msgs::PoseStampedConstPtr& goal) {
+  
+  move_base_msgs::MoveBaseGoal adaptedGoal;
+  adaptedGoal.target_pose.header.frame_id = "map";
+  adaptedGoal.target_pose.header.stamp = ros::Time::now();
+
+  adaptedGoal.target_pose.pose.position.x = goal->pose.position.x;
+  adaptedGoal.target_pose.pose.position.y = goal->pose.position.y;
+  adaptedGoal.target_pose.pose.position.z = goal->pose.position.z;
+  adaptedGoal.target_pose.pose.orientation.w = goal->pose.orientation.w;
+  
+  for (int i = 0; i < sizeof(goalList)-1; i++) {
+    if (goalList[i] == 0) {
+      goalList[i] = adaptedGoal;
+      break;
+  }
+  if (!isNavigating) navigate();
+
+}
+
+void navigate() {
+
+  isNavigating = true;
+
+  MoveBaseClient ac("move_base", true);
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  } 
+
+  for (int i = 0; i < sizeof(goalList); i++) {
+    if (goalList[i] == 0) break;
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goalList[i]);
+    ac.waitForResult();
+
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      ROS_INFO("The jackal reached the goal");
+    else
+      ROS_INFO("The jackal failed to reach the goal");
+    }
+  }
+  goalList = {0};
+    
+  move_base_msgs::MoveBaseGoal homeGoal; 
+  homeGoal.target_pose.header.frame_id = "map";
+  homeGoal.target_pose.header.stamp = ros::Time::now();
+  homeGoal.target_pose.pose.position.x = 0.0;
+  homeGoal.target_pose.pose.position.y = 0.0;
+  homeGoal.target_pose.pose.position.z = 0.0;
+  homeGoal.target_pose.pose.orientation.w = 1.0;
+
+  ROS_INFO("Sending goal");
+  ac.sendGoal(homeGoal);
+  ac.waitForResult();
+
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("The jackal reached the docking station");
+  else
+    ROS_INFO("The jackal failed to reach the docking station");
+  }
+
+  isNavigating = false;
+
+}
+
+
+
+
+int main(int argc, char **argv) {
+  
+  ros::init(argc, argv, "simple_navigation_goals");
+  ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe("cb_goal", 1000, addGoal);
+  ros::spin();
+
+  navigate();
+
+  
+
+  return 0;
+}
