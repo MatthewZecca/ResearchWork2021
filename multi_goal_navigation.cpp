eb145bd8fcc056bdab7c7fbc8dc53937af48c685
MatthewZecca <87440798+MatthewZecca@users.noreply.github.com>
2021-07-20 11:05:02 -0400

    Added comments

--- multi_goal_navigation.cpp
+++ multi_goal_navigation.cpp
@@ -2,27 +2,41 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <tf/transform_listener.h>
-#include <tf/transform_datatypes.h>
+#include <std_msgs/Bool.h>
+
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-move_base_msgs::MoveBaseGoal goalList [50];
-bool isNavigating = false;
+move_base_msgs::MoveBaseGoal goalList [50]; // setup goal list of up to 50 goals
 
-void navigate() {
+void clearGoalList () { // sets all goals to 0,0,0
+  
+  for (int i = 0; i < 50; ++i) {
+    goalList[i].target_pose.pose.position.x = 0;
+    goalList[i].target_pose.pose.position.y = 0;
+    goalList[i].target_pose.pose.position.z = 0;
+  }
+  ROS_INFO("cleared goal list");
 
-  isNavigating = true;
+}
+
+void navigate(const std_msgs::BoolConstPtr& nav) { // begins navigation
+
+  if (!nav) { // if given a false, cancel navigation and clear the goal list
+    clearGoalList();
+    return;
+  }
 
   MoveBaseClient ac("move_base", true);
   while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
+    ROS_INFO("Waiting for the move_base action server");
   } 
-
-  for (int i = 0; i < sizeof(goalList); i++) {
-    if (goalList[i].target_pose.pose.position.x == 0) break;
-    ROS_INFO("Sending goal");
+  ROS_INFO("Executing goals");
+  for (int i = 0; i < sizeof(goalList); i++) { // run through the goal list, navigating to each until the end where the jackal navigates home
+    if (goalList[i].target_pose.pose.position.x == 0 && goalList[i].target_pose.pose.position.y == 0) break;
+    ROS_INFO("Navigating to goal");
     ac.sendGoal(goalList[i]);
-    ac.waitForResult();
+    ac.waitForResult(ros::Duration(0,0));
 
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
       ROS_INFO("The jackal reached the goal");
@@ -31,7 +45,7 @@ void navigate() {
     }
   }
     
-  move_base_msgs::MoveBaseGoal homeGoal; 
+  move_base_msgs::MoveBaseGoal homeGoal; // set up the home goal at 0,0,0 for now
   homeGoal.target_pose.header.frame_id = "map";
   homeGoal.target_pose.header.stamp = ros::Time::now();
   homeGoal.target_pose.pose.position.x = 0.0;
@@ -39,10 +53,10 @@ void navigate() {
   homeGoal.target_pose.pose.position.z = 0.0;
   homeGoal.target_pose.pose.orientation.w = 1.0;
 
-  ROS_INFO("Sending goal");
+  ROS_INFO("Navigating home");
   ac.sendGoal(homeGoal);
 
-  ac.waitForResult();
+  ac.waitForResult(ros::Duration(0,0));
 
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
     ROS_INFO("The jackal reached the docking station");
@@ -50,11 +64,12 @@ void navigate() {
     ROS_INFO("The jackal failed to reach the docking station");
   }
 
-  isNavigating = false;
+  clearGoalList();
 
 }
 
-void addGoal(const geometry_msgs::PoseStampedConstPtr& goal) {
+
+void addGoal(const geometry_msgs::PoseStampedConstPtr& goal) { // adds goal to the list from the cb_goal topic
   
   move_base_msgs::MoveBaseGoal adaptedGoal;
   adaptedGoal.target_pose.header.frame_id = "map";
@@ -65,25 +80,29 @@ void addGoal(const geometry_msgs::PoseStampedConstPtr& goal) {
   adaptedGoal.target_pose.pose.position.z = goal->pose.position.z;
   adaptedGoal.target_pose.pose.orientation.w = goal->pose.orientation.w;
   
-  for (int i = 0; i < sizeof(goalList)-1; i++) {
-    if (goalList[i].target_pose.pose.position.x == 0) {
+  for (int i = 0; i < sizeof(goalList); i++) {
+    if (goalList[i].target_pose.pose.position.x == 0 && goalList[i].target_pose.pose.position.y == 0) {
       goalList[i] = adaptedGoal;
       break;
     }
   }
-  if (!isNavigating) navigate();
 
+  ROS_INFO("added goal");  
 }
+
+
 
 int main(int argc, char **argv) 
 {
   
   ros::init(argc, argv, "multi_goal_navigation");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("cb_goal", 1000, addGoal);
+  nh.setParam("move_base/actionlib_client_sub_queue_size", 30);
+  nh.setParam("move_base/actionlib_client_pub_queue_size", 30);
+  clearGoalList();
+  ros::Subscriber sub = nh.subscribe("cb_goal", 1000, addGoal);    // subscribes to "cb_goal" where our jackal goals are published
+  ros::Subscriber sub2 = nh.subscribe("navigate", 1000, navigate); // subscribes to "navigate" where the command to start the substation inspection is sent
   ros::spin();
-
-  navigate();
-
+ 
   return 0;
 }
